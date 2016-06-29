93220a57584fc63e69078a9572d156dbfb60c757
Kyle Vassella <Kyle@KellyCoiMac2016.attlocal.net>
2016-06-29 18:44:36 -0500

    project1-RandomQuoteGenerator
    Peer Review version

---
+++ js/script.js
@@ -0,0 +1,80 @@
+/***********************************************************
+This project was tested on Google Chrome, Safari & Firefox
+***********************************************************/
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);  // event listener to respond to clicks on the page. When user clicks anywhere on the page, the "makeQuote" function is called.
+
+
+var quotes = [   // an array of quote objects, one of which will display its values when printQuote() is called.
+  {quote: "A man away from home need feel no shame.", source: "Mozart", year: "1780", tag: "| inspirational", seen: false},
+  {quote: "Don't train alone - it only embeds your errors.", source: "Giralt of Rivia", citation: "The Witcher 3", tag: "| gaming", seen: false},
+  {quote: "Perseverance is failing 19 times and succeeding the 20th.", source: "Julie Andrews", tag: "| inspirational", seen: false},
+  {quote: "That's the thing that you see about great business owners..is this...undying optimism. They're realistic, but they're optimistic. Ooh, that rhymes!", source: "Robert Herjavec", citation: "Shark Tank", year: "2014", tag: "| television", seen: false},
+  {quote: "You test people under pressure. And under pressure, he was a dud.", source: "Donald J. Trump", citation: "The Apprentice", tag: "| television", seen: false},
+  {quote: "Why are dingleberries brown? That's just the way shit is.", source: "Abraham", citation: "The Walking Dead", tag: "| television", seen: false} 
+  ];
+
+var bgColors = [   // an array of different background colors to be randomly displayed when printQuote() is called.
+  "black",
+  "lime",
+  "pink",
+  "orange",
+  "crimson",
+  "salmon",
+  "purple",
+  "#0198E1"
+];
+
+//  variables to represent the values of each quotes array object's properties - for use in displaying only those properties that each object has. The j at start of the name is to help differentiate them from the quotes[] properties/keys.
+var jQuote;
+var jSource;
+var jCitation;
+var jYear;
+var jTag;   // extra credit property/key.
+var quoteObj; //variable to hold the results (return) of the getRandomQuote() function.
+var randomNumber; //variable to hold a randomly generated number.
+var counter = 0;  //  counter to keep track of how many individual quotes.seen properties have been displayed.
+
+
+function getRandomQuote () {    // generates a random number, increments counter, sets quotes.seen to 'true' and returns it. If quotes.seen is already true, while loop activates.
+
+
+randomNumber = Math.floor(Math.random() * quotes.length);    // generates and asigns a random number to a variable, to serve as the index of the quotes array.
+
+  while (quotes[randomNumber].seen) {    // if quotes.seen is false, while loop never runs. If quotes.seen is true, while loop generates a new random number and repeats until quotes.seen is false, preventing the same quote from running twice before all quotes have run.
+    if (counter == quotes.length){    // once each quote has displayed (aka all quotes.seen set to true), for loop runs which resets all quotes.seen to false and resets counter to 0.
+      for (var i=0; i < quotes.length; i++) {
+        quotes[i].seen = false;
+      }
+      counter = 0;    // reset of counter after it has reached quotes.length. This reset is critical because it prevents an endless loop. Resetting counter here prevents all quotes.seen from being perpetually true.
+    }
+    randomNumber = Math.floor(Math.random() * quotes.length);    // resets randomNumber if a quote w/ a .seen value of true appears, since a true value would mean a quote which already appeared.
+  }
+    counter++     // increments counter
+    quotes[randomNumber].seen = true;    //sets quote.seen to true for use in while loop.
+    return quotes[randomNumber];         
+}
+
+setInterval(printQuote, 30000);   //automatically runs the printQuote() function every 30 seconds.
+
+
+function printQuote () {    // calls the getRandomQuote() function and stores the returned quote object in a variable (var quoteObj). Constructs a string using the different properties of the quote object. Manipulates the DOM to display the final HTML string ot the page.
+  quoteObj = getRandomQuote();
+  jQuote = quoteObj.quote
+  jSource = quoteObj.source
+  jCitation = quoteObj.citation
+  jYear = quoteObj.year
+  jTag = quoteObj.tag
+    
+    document.body.style.backgroundColor = bgColors[Math.floor(Math.random() * bgColors.length)];       // background color generator which randomly selects a background color from the bgColors array.
+
+  if (jCitation != null && jYear != null){        // conditional statements which print only those properties/values that the selected quote object has.
+      document.getElementById("quote-box").innerHTML = '<p class="quote">' + jQuote + '</p>' + '<p class = "source"> ' + jSource + '<span class = "citation"> ' + jCitation + ' </span> <span class="year"> ' + jYear + ' </span> <span class="tag"> ' + jTag + ' </span></p>'
+  } else if (jCitation != null) {
+      document.getElementById("quote-box").innerHTML = '<p class="quote">' + jQuote + '</p>' + '<p class = "source"> ' + jSource + '<span class = "citation"> ' + jCitation + ' </span> <span class="tag"> ' + jTag + ' </span></p>';
+  } else if (jYear != null) {
+      document.getElementById("quote-box").innerHTML = '<p class="quote">' + jQuote + '</p>' + '<p class = "source"> ' + jSource + '<span class="year"> ' + jYear + ' </span> <span class="tag"> ' + jTag + ' </span></p>';
+  } else {
+      document.getElementById("quote-box").innerHTML = '<p class="quote">' + jQuote + '</p>' + '<p class = "source"> ' + jSource + '<span class="tag"> ' + jTag + ' </span></p>';
+  }
+}
